e0e63e1b302c51cdb5f1dfcb5a0c5e1271dfaefc
Karla Lockhart <kblockhart@gmail.com>
2024-03-04 14:23:15 -0800

    passed tests

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
+#from vehicle import Vehicle
+
+#class Car(Vehicle):
+#    pass
+
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
